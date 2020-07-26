b23f4b7729a939ad1eb7608c35639924b588a3bc
sultan254 <abubakarali3w@gmail.com>
2020-07-26 15:52:29 +0300

    Initial version

---
+++ app.js
@@ -0,0 +1,51 @@
+function refresh(){
+    var url =   "http://api.weatherstack.com/current?access_key=48d1fa0a786daf92e44a514a04759b1d&query=fetch:ip"
+
+    function callback(data){
+        console.log(data)
+        $(".country").html(data.location.timezone_id);
+        $(".country").append(`
+            <img src="${data.current.weather_icons[0]}" class="img-fluid"></img>
+        `)
+        $(".temperature").html(data.current.temperature + "°C")
+        $(".description").html(data.current.weather_descriptions)
+        $("button").click((e)=>{
+            let input = e.target
+            input.disabled = true
+            isVisible = false
+            let visibility = isVisible ? "hidden" : "visible"
+            $(".more").css("visibility", visibility)
+            $(".more").append(`
+                <table class="table">
+                        <tr>
+                            <th >Humidity</th>
+                            <th>${data.current.humidity}</th>
+                        </tr>
+                        <tr>
+                            <th >Observational Time</th>
+                            <th>${data.current.observation_time}</th>
+                        </tr>
+                        <tr>
+                            <th >Wind Speed</th>
+                            <th>${data.current.wind_speed}</th>
+                        </tr>
+                        <tr>
+                            <th >Wind Direction</th>
+                            <th>${data.current.wind_dir}</th>
+                        </tr>
+                    <tbody>
+                    </tbody>
+                </table>
+
+
+            `)
+        })
+
+
+
+    }
+    $.getJSON(url,callback)
+}
+
+
+
